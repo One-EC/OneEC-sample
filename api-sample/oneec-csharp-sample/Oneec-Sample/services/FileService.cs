127de4a9979861acd66a5ae322cf94166d70e2c2
OneEC <104045956+One-EC@users.noreply.github.com>
2023-09-21 12:17:43 +0800

    add file service

---
+++ api-sample/oneec-csharp-sample/Oneec-Sample/services/FileService.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Oneec_Sample.models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Oneec_Sample.services
+{
+    class FileService
+    {
+        internal void DownloadtoLocal(string rootPath, string data)
+        {
+            var shipDocument = JsonConvert.DeserializeObject<ShipDocumentModel>(data);
+            var realPath = Path.Combine(rootPath, shipDocument.fileName);
+            var dataByte = Convert.FromBase64String(shipDocument.data);
+            File.WriteAllBytes(realPath, dataByte);
+
+        }
+    }
+}
