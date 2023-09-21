68a8ea08f77d63272015da63c4df3160db7bb15d
OneEC <104045956+One-EC@users.noreply.github.com>
2023-09-21 12:18:12 +0800

    add file service

---
+++ api-sample/oneec-csharp-sample/Oneec-Sample/models/ShipDocumentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oneec_Sample.models
+{
+    class ShipDocumentModel
+    {
+        public string fileName { get; set; }
+        public int size { get; set; }
+        public string data { get; set; }
+    }
+
+
+}
